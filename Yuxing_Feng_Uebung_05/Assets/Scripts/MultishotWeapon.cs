b215a2b7da4fa5816e2ed0fbcdfc0bc8b96e67f1
YuxingF <yuxing.feng@stud.tu_darmstadt.de>
2020-06-01 10:44:53 +0800

    first commit

---
+++ Yuxing_Feng_Uebung_05/Assets/Scripts/MultishotWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultishotWeapon : Weapon
+{
+    float degree = .1f;
+    //this.audios = audios;
+
+    protected override void Shoot(ProjectilePool pool, Vector2 position,  Vector2 direction){
+        pool.GetProjectile(this._transform.position, direction);
+        pool.GetProjectile(this._transform.position, direction.Rotate(20f));
+        pool.GetProjectile(this._transform.position, direction.Rotate(-20f));
+    }
+}
+
+
